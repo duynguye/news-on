5af195550011afc07c8477f9904efce4f032b47e
Duy Nguyen <andy@tier3interactive.com>
2019-12-26 16:30:14 -0500

    UpdatE

--- pages/api/process-form-submissions.js
+++ pages/api/process-form-submissions.js
@@ -2,28 +2,28 @@ import fetch from 'isomorphic-unfetch'
 const API_ENDPOINT = 'https://sbgi118262site.wpengine.com/wp-json/frm/v2/forms/1/entries';
 
 export default (req, res) => {
-  // let message = req.body;
-  // let interests = [];
+  let message = req.body;
+  let interests = [];
   
-  // if (message.addstation) {
-  //   interests.push('Add your TV station');
-  // }
+  if (message.addstation) {
+    interests.push('Add your TV station');
+  }
   
-  // if (message.advertiser) {
-  //   interests.push('Become an advertiser');
-  // }
+  if (message.advertiser) {
+    interests.push('Become an advertiser');
+  }
   
-  // if (message.press) {
-  //   interests.push('Press inquiry');
-  // }
+  if (message.press) {
+    interests.push('Press inquiry');
+  }
   
-  // if (message.join) {
-  //   interests.push('Join the team');
-  // }
+  if (message.join) {
+    interests.push('Join the team');
+  }
   
-  // if (message.other) {
-  //   interests.push('Other');
-  // }
+  if (message.other) {
+    interests.push('Other');
+  }
 
   res.status(200).send(JSON.stringify(req.body));
   
