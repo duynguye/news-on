56dea48a5a26cca957d338827b16286af2dcd8b0
Duy Nguyen <andy@tier3interactive.com>
2019-12-26 15:31:11 -0500

    Removed CORS setup

--- pages/api/process-form-submissions.js
+++ pages/api/process-form-submissions.js
@@ -2,58 +2,49 @@ import fetch from 'isomorphic-unfetch'
 const API_ENDPOINT = 'https://sbgi118262site.wpengine.com/wp-json/frm/v2/forms/1/entries';
 
 export default async (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-
-  if (req.method === 'OPTIONS') {
-    res.set('Access-Control-Allow-Methods', 'GET');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
-    res.set('Access-Control-Max-Age', '3600');
-    res.status(204).send('');
+  let message = req.body;
+  let interests = [];
+  
+  if (message.addstation) {
+    interests.push('Add your TV station');
+  }
+  
+  if (message.advertiser) {
+    interests.push('Become an advertiser');
+  }
+  
+  if (message.press) {
+    interests.push('Press inquiry');
+  }
+  
+  if (message.join) {
+    interests.push('Join the team');
+  }
+  
+  if (message.other) {
+    interests.push('Other');
+  }
+  
+  let results = await fetch(API_ENDPOINT, {
+    method: 'POST',
+    headers: { 
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic OURQQy1ITVlNLUQ4VkktMURCRzp4'
+    },
+    body: JSON.stringify({
+      "qh4icy": message.fullname,
+      "8igzw": message.title,
+      "kzex8": message.company,
+      "29yf4d": message.email,
+      "vfxqe": message.phone,
+      "9jv0r1": message.message,
+      "rcft7": interests
+    })
+  });
+  
+  if (results.ok) {
+    res.status(200).send(results);
   } else {
-    let message = req.body;
-    let interests = [];
-    
-    if (message.addstation) {
-      interests.push('Add your TV station');
-    }
-    
-    if (message.advertiser) {
-      interests.push('Become an advertiser');
-    }
-    
-    if (message.press) {
-      interests.push('Press inquiry');
-    }
-    
-    if (message.join) {
-      interests.push('Join the team');
-    }
-    
-    if (message.other) {
-      interests.push('Other');
-    }
-    
-    let results = await fetch(API_ENDPOINT, {
-      method: 'POST',
-      headers: { 
-        'Content-Type': 'application/json',
-      	'Authorization': 'Basic OURQQy1ITVlNLUQ4VkktMURCRzp4'
-      },
-      body: JSON.stringify({
-      	"qh4icy": message.fullname,
-        "8igzw": message.title,
-        "kzex8": message.company,
-        "29yf4d": message.email,
-        "vfxqe": message.phone,
-        "9jv0r1": message.message,
-        "rcft7": interests
-      })
-    });
-    
-    if (results.ok) {
-      res.status(200).send(results);
-    } else {
-      res.status(500).send(results.statusText);
-    }
+    res.status(500).send(results.statusText);
   }
 }
