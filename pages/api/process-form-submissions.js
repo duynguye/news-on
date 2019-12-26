d13442934e4b83fde83249c5eb02cb6686507921
Duy Nguyen <andy@tier3interactive.com>
2019-12-26 16:33:44 -0500

    Testing

--- pages/api/process-form-submissions.js
+++ pages/api/process-form-submissions.js
@@ -24,29 +24,27 @@ export default (req, res) => {
   if (message.other) {
     interests.push('Other');
   }
-
-  res.status(200).send(JSON.stringify(req.body));
   
-  // fetch('https://sbgi118262site.wpengine.com/wp-json/frm/v2/forms/1/entries', {
-  //   method: 'POST',
-  //   headers: { 
-  //     'Content-Type': 'application/json',
-  //     'Authorization': 'Basic OURQQy1ITVlNLUQ4VkktMURCRzp4'
-  //   },
-  //   body: JSON.stringify({
-  //     "qh4icy": message.fullname,
-  //     "8igzw": message.title,
-  //     "kzex8": message.company,
-  //     "29yf4d": message.email,
-  //     "vfxqe": message.phone,
-  //     "9jv0r1": message.message,
-  //     "rcft7": interests
-  //   })
-  // }).then(response => {
-  //   if (response.ok) {
-  //     res.status(200).send('Success');
-  //   } else {
-  //     res.status(response.status).send(JSON.stringify(req));
-  //   }
-  // });
+  fetch('https://sbgi118262site.wpengine.com/wp-json/frm/v2/forms/1/entries', {
+    method: 'POST',
+    headers: { 
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic OURQQy1ITVlNLUQ4VkktMURCRzp4'
+    },
+    body: JSON.stringify({
+      "qh4icy": message.fullname,
+      "8igzw": message.title,
+      "kzex8": message.company,
+      "29yf4d": message.email,
+      "vfxqe": message.phone,
+      "9jv0r1": message.message,
+      "rcft7": interests
+    })
+  }).then(response => {
+    if (response.ok) {
+      res.status(200).send('Success');
+    } else {
+      res.status(response.status).send(JSON.stringify(req));
+    }
+  });
 }
